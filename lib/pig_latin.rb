9469658cb9ffed09c8e181b029293fa1e63721ef
Christine Oen <oen.christine@gmail.com>
2014-07-28 22:59:58 -0500

    changed to return statements

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -6,13 +6,13 @@ module PigLatin
   vowels=['a','e','i','o','u']
   consonants= alphabet - vowels
     if vowels.include?(word[0])
-      puts word + "way"
+      return word + "way"
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
-      puts word[2..-1] + word[0..1]+ "ay"
+      return word[2..-1] + word[0..1]+ "ay"
     elsif consonants.include?(word[0])
-      puts word[1..-1] + word[0]+ "ay"
+      return word[1..-1] + word[0]+ "ay"
     else
-      puts "That's not a word."     
+      return "That's not a word."     
     end
   end
 end
